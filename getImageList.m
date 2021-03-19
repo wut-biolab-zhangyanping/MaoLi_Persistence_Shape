20482dcd65f23c018ecbf05375e861f8636131d0
zhangyanping <1415775989@qq.com>
2021-03-19 18:29:02 +0800

    soybean

--- getImageList.m
+++ getImageList.m
@@ -1,10 +1,10 @@
 function [train_img_path, train_label,val_img_path, val_label, test_img_path, test_label] = getImageList()
-        [train_img_list, train_y_list] = importfile('/home/zyp/MFCIS/train_and_test_file/cherry_train_file_0_not_aug.csv');
-        [val_img_list, val_y_list] = importfile('/home/zyp/MFCIS/train_and_test_file/cherry_val_file_list_0.csv');
-        [test_img_list, test_y_list] = importfile('/home/zyp/MFCIS/train_and_test_file/cherry_test_file_0.csv');
+        [train_img_list, train_y_list] = importfile('/home/zyp/MFCIS/train_and_test_file/soybean/R1/soybean_train_file_0.csv');
+        [val_img_list, val_y_list] = importfile('/home/zyp/MFCIS/train_and_test_file/soybean/R1/soybean_val_file_list_0.csv');
+        [test_img_list, test_y_list] = importfile('/home/zyp/MFCIS/train_and_test_file/soybean/R1/soybean_test_file_0.csv');
         train_img_file = train_img_list;
         train_y = train_y_list;
-        [class, label] = importMapFile('/home/zyp/MFCIS/cherry_id.txt');
+        [class, label] = importMapFile('/home/zyp/MFCIS/soybean_id.txt');
         for i = 1:length(train_y)
             for j = 1:length(class)
                 if train_y(i) == class(j)
